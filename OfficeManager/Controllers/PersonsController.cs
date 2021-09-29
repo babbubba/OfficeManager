f92e90bc208e7698825439205ddd275d443e3ff6
Babbubba <fcavallari@bsoftsolutions.it>
2021-09-29 15:23:57 +0200

    Aggiornati commenti per swagger

--- OfficeManager/Controllers/PersonsController.cs
+++ OfficeManager/Controllers/PersonsController.cs
@@ -1,5 +1,6 @@
 ﻿using bs.Data.Helpers;
 using bs.Data.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OfficeManager.Interfaces;
 using OfficeManager.ViewModels;
@@ -10,6 +11,7 @@ namespace OfficeManager.Controllers
     /// 
     /// </summary>
     /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class PersonsController : ControllerBase
@@ -40,7 +42,7 @@ namespace OfficeManager.Controllers
         /// <remarks>
         /// Richiesta di esempio:
         ///
-        ///     POST /Persons
+        ///     POST /api/persons
         ///     {
         ///        "name": "Mario",
         ///        "surname": "Rossi",
@@ -50,6 +52,10 @@ namespace OfficeManager.Controllers
         /// </remarks>
         /// <param name="personViewModel">The person view model.</param>
         /// <returns></returns>
+        /// <response code="201">Returns the newly created person id</response>
+        /// <response code="500">If any error occurs</response>            
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost()]
         public IActionResult CreatePerson(PersonViewModel personViewModel)
         {
@@ -62,8 +68,19 @@ namespace OfficeManager.Controllers
         /// <summary>
         /// Deletes the person.
         /// </summary>
+        /// <remarks>
+        /// Richiesta di esempio:
+        ///
+        ///     DELETE /api/persons/-personId-
+        ///
+        /// NB.: Sostituisci -personId- con l'identificatifo della persona (id) che vuoi eliminare
+        /// </remarks>
         /// <param name="id">The identifier.</param>
         /// <returns></returns>
+        /// <response code="200">Returns a boolean that explains if the person was deleted or not</response>
+        /// <response code="500">If any error occurs</response>            
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpDelete("{id}")]
         public IActionResult DeletePerson(string id)
         {
@@ -76,7 +93,19 @@ namespace OfficeManager.Controllers
         /// <summary>
         /// Gets all persons.
         /// </summary>
+        /// <remarks>
+        /// Richiesta di esempio:
+        ///
+        ///     GET /api/persons
+        ///
+        /// </remarks>
         /// <returns></returns>
+        /// <response code="200">Returns the list of persons</response>
+        /// <response code="204">No persons exist</response>
+        /// <response code="500">If any error occurs</response>            
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpGet]
         public IActionResult GetAllPersons()
         {
@@ -89,8 +118,21 @@ namespace OfficeManager.Controllers
         /// <summary>
         /// Gets the person by identifier.
         /// </summary>
+        /// <remarks>
+        /// Richiesta di esempio:
+        ///
+        ///     GET /api/persons/-personId-
+        ///
+        /// NB.: Sostituisci -personId- con l'identificatifo della persona (id) che vuoi ottenere
+        /// </remarks>
         /// <param name="id">The identifier.</param>
         /// <returns></returns>
+        /// <response code="200">Returns the person</response>
+        /// <response code="204">No person exists</response>
+        /// <response code="500">If any error occurs</response>            
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpGet("{id}")]
         public IActionResult GetPersonById(string id)
         {
@@ -103,8 +145,25 @@ namespace OfficeManager.Controllers
         /// <summary>
         /// Updates the person.
         /// </summary>
+        /// <remarks>
+        /// Richiesta di esempio:
+        ///
+        ///     PUT /api/persons
+        ///     {
+        ///        "id": -personId-
+        ///        "name": "Mario",
+        ///        "surname": "Rossi",
+        ///        "birthDate": "1990-06-10"
+        ///     }
+        ///
+        /// NB.: Sostituisci -personId- con l'identificatifo della persona (id) che vuoi modificare
+        /// </remarks>
         /// <param name="personViewModel">The person view model.</param>
         /// <returns></returns>
+        /// <response code="200">Returns a boolean that explains if the person was updated or not</response>
+        /// <response code="500">If any error occurs</response>            
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPut()]
         public IActionResult UpdatePerson(PersonViewModel personViewModel)
         {
